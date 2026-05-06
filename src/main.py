9d45def53f8fcef450590d064e38285668a33f38
Erick Daniel <erickguitar2015@gmail.com>
2026-05-06 00:24:17 -0600

    Prueba API con FastApi y pipeline ci con matrix y artefactos prueba2

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ app = FastAPI()
 # base de dartos falsa en memoria
 
 productos = {
-    1: {"id": 1, "nombre": "Laptop", "precio": 999.99},
-    2: {"id": 2, "nombre": "Mouse", "precio": 29.99},
-    3: {"id": 3, "nombre": "Teclado", "precio": 49.99},
+    1: {"id":1, "nombre":"Laptop", "precio":999.99},
+    2: {"id":2, "nombre":"Mouse", "precio":29.99},
+    3: {"id":3, "nombre":"Teclado", "precio":49.99},
 }
 
 
@@ -15,15 +15,18 @@ productos = {
 def root():
     return {"mensaje":"API Funcionando"}
 
+
 @app.get("/productos")
 def obtener_productos():
     return list(productos.values())
 
+
 @app.get("/productos/{id}")
-def obtener_productos(id: int):
+def obtener_producto(id: int):
     if id not in productos:
         raise HTTPException(status_code=404, detail="Producto no encontrado")
     return productos[id]
+
 
 @app.post("/productos")
 def crear_producto(producto: dict):
